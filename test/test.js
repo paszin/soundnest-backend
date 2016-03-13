642e3932a46521ee16847178590056de7b4222c5
Pascal Crenzin <pascalcrenzin@aol.de>
2016-03-13 15:38:10 -0700

    test for history

--- test/test.js
+++ test/test.js
@@ -4,6 +4,7 @@ var server = require("../server.js");
 server.startServer("soundnest-test");
 var Groups = require("../groups/groups-model.js");
 var Invitations = require("../invitations/invitations-model.js");
+var History = require("../history/history-model.js");
 
 
 describe("Basics", function() {
@@ -113,13 +114,53 @@ describe("Invitation", function() {
 	});
 
 	it("sould be member of the new group", function(done) {
-		Groups.find({"members.id": 200}).then(function(groups) {
+		Groups.find({
+			"members.id": 200
+		}).then(function(groups) {
 			expect(groups).to.be.a("array").with.length(1);
 			expect(groups[0]).to.have.property("id", newgroup.id);
 			done();
 		});
 	})
+});
 
 
+describe("History", function() {
 
+	before(function(done) {History.remove({}).then(function() {done()})});
+
+	var options = {};
+	options.url = "/history";
+
+	it("should add a track to the History", function(done) {
+		options.method = "POST";
+		options.payload = {
+			user_id: 100,
+			track_id: 1001,
+			statistics: {
+				comment_count: 400,
+				playback_count: 40,
+				favoritings_count: 4,
+				play_status: 4000
+			}
+		};
+		server.server.inject(options, function(response) {
+			expect(response.statusCode).to.equal(201);
+			done();
+		});
+	});
+
+	it ("should return all tracks in the history", function(done) {
+		options.method = "GET";
+		options.payload = null;
+		options.url += "?user_id=100";
+		server.server.inject(options, function(response) {
+			expect(response.statusCode).to.equal(200);
+			expect(response.result).to.have.property("tracks").with.length(1);
+			expect(response.result.tracks).to.be.a("array");
+			expect(response.result.tracks[0]).to.have.property("sn");
+			expect(response.result.tracks[0].sn.track_id).to.equal(1001);
+			done();
+		});
+	});
 });
